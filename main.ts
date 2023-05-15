851a7102395a55c9ee513cc20b0b27183227b74c
Chamsseddine03 <132678444+Chamsseddine03@users.noreply.github.com>
2023-05-15 10:56:03 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+function base () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    strip.show()
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+strip.setBrightness(5)
+base()
+basic.forever(function () {
+    for (let index = 0; index <= 25; index++) {
+        strip.setBrightness(0 * index)
+        base()
+        basic.pause(100)
+    }
+})
